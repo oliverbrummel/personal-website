809a3ef36017dbb0eddecc16caf4b7ccb96ab54d
Oliver Brummel <oliverbrummel1@gmail.com>
2019-09-21 18:37:52 -0500

    Add new TerminalComponent and route and move styles there.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TerminalComponent } from './terminal/terminal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TerminalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
