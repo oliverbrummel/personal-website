2daca5f36bc4201224bb4275df780fac1a4f5d27
Oliver Brummel <oliverbrummel1@gmail.com>
2019-09-21 21:59:06 -0500

    Get hidden input mostly working

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-terminal',
@@ -6,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminal.component.scss']
 })
 export class TerminalComponent implements OnInit {
+
+  @ViewChild('hiddenInput') hiddenInput: ElementRef;
+
+  hiddenInputEl = new FormControl('');
+
   currentDateTime: Date;
   userInput: string;
+  displayedOutput = '';
 
   constructor() { }
 
   ngOnInit() {
     this.currentDateTime = new Date();
+    console.log('this', this);
+
+    this.subscribeToInputChanges();
+  }
+
+  subscribeToInputChanges() {
+    this.hiddenInputEl.valueChanges.subscribe(value => {
+      const lastCharacter = value.slice(-1);
+      this.displayedOutput += lastCharacter;
+
+      // if (lastCharacter === ' ') {
+      //   console.log('WE GOTTA SPACE!');
+      // }
+    });
+  }
+
+  setFocus() {
+    this.hiddenInput.nativeElement.focus();
   }
 
 }
